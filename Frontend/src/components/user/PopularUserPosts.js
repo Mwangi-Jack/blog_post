fd5bef4505732ebfcbec0bbea6939df3b55da633
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-09-12 18:43:26 +0300

    improved the responsitivity of the navbar on the user dashboard

--- Frontend/src/components/user/PopularUserPosts.js
+++ Frontend/src/components/user/PopularUserPosts.js
@@ -7,7 +7,7 @@ function PopularUserPostsView() {
 		<div className="my-16 mx-4">
 			<h1 className="text-xl font-bold">Your Popular Posts</h1>
 			<hr className="h-[2px] my-1 bg-[#7C4EE4]" />
-			<div className="my-6 md:grid md:grid-cols-4 md:gap-16">
+			<div className="my-6 md:grid md:grid-cols-4 md:gap-4">
 				{PopularUserPosts.map((post, index) => <Card post={post} key={index} edit={true}/> )}
 			</div>
 		</div>
