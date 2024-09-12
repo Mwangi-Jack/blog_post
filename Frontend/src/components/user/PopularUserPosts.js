74e9b7958d0fb2b0276ff8adc3857137b9bdffb6
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-09-12 15:29:27 +0300

    created a reach text editor to allow user write blog

--- Frontend/src/components/user/PopularUserPosts.js
+++ Frontend/src/components/user/PopularUserPosts.js
@@ -1,35 +1,5 @@
-import { LiaReadme } from "react-icons/lia";
-import { FaRegEye } from "react-icons/fa6";
 import { PopularUserPosts } from '../../utils/Posts';
 import Card from "../UI/Card";
-
-function PostRow({ post, index }) {
-	return (
-		<div className="flex justify-between border-b pb-2">
-			<div className="flex space-x-4">
-				<div className="h-12 w-12">
-					<img src={post.pic} className="rounded-full"  alt='Blog' />
-				</div>
-				<div>
-					<h1>{post.title}</h1>
-					<div className="flex space-x-4">
-						<span className="text-sm text-gray-500">{post.date}</span>
-						<div className="flex space-x-4">
-							{post.tags.map((tag) => (
-								<div className="border px-1 rounded-lg text-sm">{tag}</div>
-							))}
-						</div>
-					</div>
-				</div>
-			</div>
-			<div className="flex items-center space-x-1 w-16 text-sm">
-				<FaRegEye size={12} />
-				<span>{post.views}</span>
-				{index + 1}
-			</div>
-		</div>
-	);
-};
 
 
 function PopularUserPostsView() {
--- Frontend/src/components/user/ViewershipChart.js
+++ Frontend/src/components/user/ViewershipChart.js
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 import { data } from '../../utils/Viewership';
 
